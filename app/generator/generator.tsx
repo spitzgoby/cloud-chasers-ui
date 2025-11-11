2ac27e53e87fd74c3cbe891c3dd548be7af9deaf
Tom Leu <thomas.leu@wnco.com>
2025-11-10 20:04:52 -0800

    Update generator.tsx

--- app/generator/generator.tsx
+++ app/generator/generator.tsx
@@ -89,7 +89,7 @@ export const Generator = () => {
     try {
       const apiUrl = import.meta.env.DEV 
         ? "/api" 
-        : "https://pf3w7890x3.execute-api.us-east-1.amazonaws.com/dev/{proxy+}";
+        : "https://4gyqqb0c3m.execute-api.us-east-1.amazonaws.com";
       
       const response = await fetch(apiUrl, {
         method: "POST",
